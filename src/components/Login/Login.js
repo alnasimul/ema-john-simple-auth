9a7bc8dcc9dc8c89e8b33b109270932c6dbe5e8d
Al Nasimul Haque <alnasimul@gmail.com>
2021-03-14 22:36:44 +0600

    Authentication feature added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,160 @@
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
+import firebaseConfig from "./firebase.config";
+import { UserContext } from '../../App';
+
+
+
+
+firebase.initializeApp(firebaseConfig);
+function Login() {
+  const [newUser,setNewUser] = useState(false);
+  const [user,setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: '' ,
+    error:'',
+    success: false
+  })
+
+  const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+  const provider = new firebase.auth.FacebookAuthProvider();
+
+  const handelFbSignIn = () => {
+  firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+   
+    var credential = result.credential;
+
+   
+    var user = result.user;
+
+    
+    var accessToken = credential.accessToken;
+    setLoggedInUser(user);
+    console.log('after sign in facebook',user);
+
+  })
+  .catch((error) => {
+    
+    var errorCode = error.code;
+    var errorMessage = error.message;
+   
+    var email = error.email;
+    
+    var credential = error.credential;
+
+  });
+  }
+
+  const handelBlur = (e) => {
+    let isFieldValid = true;
+    if(e.target.name === "email"){
+        const re = /\S+@\S+\.\S+/;
+        isFieldValid = re.test(e.target.value);
+        console.log(isFieldValid);
+    }else if(e.target.name === "password"){
+        const isPasswordValid = e.target.value.length > 6 ;
+        const re = /\d{1}/;
+        const passwordHasNumber = re.test(e.target.value);
+        isFieldValid = isPasswordValid && passwordHasNumber;
+        console.log(isFieldValid);
+    }
+    if(isFieldValid){
+      const newUserInfo = {...user}
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+     // console.log(user);
+
+    }
+  } 
+
+  const handelSubmit = (e) => {
+   // console.log(user.email,user.password);
+      if(newUser && user.email && user.password){
+          firebase.auth().createUserWithEmailAndPassword(user.email,user.password)
+          .then((res) => {
+            const newUserInfo = {...user}
+            newUserInfo.error = '';
+            newUserInfo.success = true;
+            setUser(newUserInfo);
+            //  setLoggedInUser(newUserInfo);
+            //console.log('sign in user info',res.user);
+          
+          })
+          .catch((error) => {
+            const newUserInfo = {...user}
+            newUserInfo.error = error.message;
+            newUserInfo.success = false;
+            setUser(newUserInfo);
+            // var errorCode = error.code;
+            // var errorMessage = error.message;
+            // console.log(errorCode,errorMessage);
+          });
+      }
+      if(!newUser && user.email && user.password){
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          const newUserInfo = {...user}
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          setLoggedInUser(newUserInfo);
+          updateUserName(user.name);
+        })
+        .catch(error => {
+          const newUserInfo = {...user}
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        })
+      }
+      e.preventDefault();
+  }
+
+  const updateUserName = name => {
+    const user = firebase.auth().currentUser;
+    
+    console.log(user);
+
+    user.updateProfile({
+      displayName: name,
+    }).then(function() {
+      console.log('username updated successfully')
+    }).catch(function(error) {
+      console.log(error)
+    });
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handelFbSignIn}>Sign in using Facebook</button>
+
+      <h1>Our own Authentication</h1>
+      <p>Name: {user.name}</p>
+      <p>Email : {user.email}</p>
+      <p>Password : {user.password}</p>
+      <input type="checkbox" onChange={ () => setNewUser(!newUser)} name="newUser" id=""/>
+      <label htmlFor="newUser">New User Sign up</label>
+      <form onSubmit={handelSubmit}>
+        {newUser &&  <input type="text" name="name" placeholder="Your Name" onBlur={handelBlur} required/> } 
+        <br/>
+        <input type="text" name="email" placeholder="Your Email Address" required onBlur={handelBlur} />
+        <br />
+        <input type="password" name="password" id="" placeholder="Your Password" required onBlur={handelBlur} />
+        <br />
+        <input type="submit" value={newUser ? 'Sign up' : 'Sign in'}/>
+      </form>
+      <p style={{color : 'red'}}>{user.error}</p>
+      {user.success && <p style={{color:'green'}}>User {newUser ? 'Created' : 'Logged in'} Successfully</p>}
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Shipment/Shipment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shipment.css';
+
+
+const Shipment = () => {
+    return (
+        <div>
+             <h1>This is shipment</h1>
+        </div>
+    );
+};
+
+export default Shipment;
